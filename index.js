05d1c1c5896a4fe87abace923c13ba05e178f5ef
William Luck <luck.william.h@gmail.com>
2022-03-23 09:55:40 -0500

    commit tree d839ad2947f82a9c4fc19dff82b7c9ed3819123b

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+// don't want to put clickAlert() here because we just want to reference the function, not invoke it immediately. If we do put the (), the function will just be invoked automatically. 
+input.addEventListener('click', clickAlert);
